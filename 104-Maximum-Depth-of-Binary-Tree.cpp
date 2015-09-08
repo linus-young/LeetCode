66c910ab4e83b5187bf52a8b7dd4f59a35393ad5
linus-young <ylm19921123@msn.cn>
2015-09-09 00:39:57 +0800

    104-Maximum-Depth-of-Binary-Tree.cpp solved

---
+++ 104-Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        else if(root->left == NULL && root->right == NULL)
+            return 1;
+        else {
+        	// If you don't use local variable to store left-sub-tree depth and right-sub-tree depth, and compare them in a single line of code like this:
+
+        	// return 1 + ((maxDepth(root->left) > maxDepth(root->right)) ? maxDepth(root->left) : maxDepth(root->right));
+
+        	// You'll get TLE error :(
+
+        	int left_depth = maxDepth(root->left);
+        	int right_depth = maxDepth(root->right);
+
+        	return 1 + ((left_depth > right_depth) ? left_depth : right_depth);
+        }
+    }
+};
